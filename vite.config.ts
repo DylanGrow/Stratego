2a7d67673741c99aaee96948dae3ab4a64032c04
Skylar Grow <dylangrow@gmail.com>
2026-05-25 18:59:39 -0400

    Fix base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
+  base: '/Stratego/',
   plugins: [
     VitePWA({
       registerType: 'autoUpdate',
